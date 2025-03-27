0b4db22b2f33b44ab205d03106a30bb6b00a61ac
Abdullahi Mohamed <abdullahi.mohamed0248@gmail.com>
2025-03-27 00:53:00 -0400

    variadic functions 101

--- va101.c
+++ va101.c
@@ -1,11 +1,13 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+// VARIABLE Length arrays / variadic functions 101
+
 void print_nums(int count, ...) {
-	va_list args;
+	va_list args; // a sort of list that deals with the "..." parameter
 	va_start(args, count);
-	vfprintf(stdout, "Num %d: %d, %d\n", args);
-	va_end(args);
+	vfprintf(stdout, "Num %d: %d, %d\n", args); // printf "under the hood"
+	va_end(args); // cleanup
 }
 
 int main() {
